89a779cda00b3dd65f73cfbac118b395b621f336
Marco Farina <marco.farina@jcmaxwell.it>
2024-10-22 01:31:13 +0200

    First section ported (Programmare in Python)

---
+++ docs/intro.md
@@ -0,0 +1,21 @@
+---
+sidebar_position: 1
+---
+
+# Introduzione
+
+## Perché questo libro?
+
+Questo libro nasce in risposta alla crescente necessità di modernizzare l'insegnamento del coding per gli studenti delle scuole superiori, e in particolare l'indirizzo "Informatica" degli Istituti Tecnici e l'articolazione "Scienze Applicate" del Liceo Scientifico. I libri di testo cartacei tradizionali risultano ormai antiquati nel contesto informatico in rapidissima evoluzione, mentre un approccio innovativo come un sito dinamico e open source può avere una chance di rimanere al passo con il rapido evolversi del mondo.
+
+Il passaggio a un formato digitale offre diversi vantaggi. In primo luogo, un libro _open source_ rende possibile adattare i contenuti alle proprie necessità per chiunque voglia farlo. Il libro può essere aggiornato facilmente e quasi "in tempo reale", riflettendo gli ultimi sviluppi nel campo dell'informatica e del linguaggio, garantendo che gli studenti abbiano accesso alla versione più aggiornata e pertinente del corso in ogni momento.
+
+Oltre a ciò, l'approccio digitale permette l'integrazione di elementi multimediali, arricchendo la fruizione del libro. Si possono incorporare video, esempi interattivi e altri materiali che favoriscano un apprendimento più coinvolgente e pratico.
+
+Un altro vantaggio significativo è la natura pubblica e accessibile del sito. Gli studenti possono accedervi da qualsiasi luogo e momento, eliminando le limitazioni spaziali e temporali associate ai libri di testo tradizionali. Questa accessibilità è cruciale per favorire un apprendimento continuo e autonomo.
+
+## Perché Python?
+
+Questo libro è progettato per fornire agli studenti una solida base nelle logiche della programmazione linguaggio Python. I capitoli sono strutturati per essere accessibili anche a chi si avvicina per la prima volta all'informatica, partendo dai meccanismi che stanno alla base del _problem solving_.
+
+Non sono quindi necessarie conoscenze pregresse di programmazione, ma è consigliato avere una buona comprensione dei concetti matematici di base e un computer per svolgere gli esercizi.
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,20 +3,20 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  title: 'Python Doesn\'t Byte',
+  tagline: 'Programmare in Python senza farsi male',
+  favicon: 'img/icons/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://www.rainbowbits.cloud',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Rainbow Bits', // Usually your GitHub org/user name.
+  projectName: 'python-doesnt-byte', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -25,8 +25,8 @@ const config: Config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'it',
+    locales: ['it'],
   },
 
   presets: [
@@ -38,7 +38,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/marcofarina/python-doesnt-byte',
         },
         blog: {
           showReadingTime: true,
@@ -48,8 +48,8 @@ const config: Config = {
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          //editUrl:
+          //  'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
@@ -129,7 +129,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Python Doesn't Byte from Rainbow Bits. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
